b48f2cd805162050b2a46e19c27ed54f23514574
madhanvamsi <madhanvamsi27@gmail.com>
2023-10-15 10:50:09 +0530

    crudOperations

--- main/java/org/jsp/UserBootApp/dto/Product.java
+++ main/java/org/jsp/UserBootApp/dto/Product.java
@@ -2,6 +2,7 @@ package org.jsp.UserBootApp.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,7 +21,9 @@ public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Column(nullable = false)
 	private String name, brand, category, description;
+	@Column(nullable = false)
 	private double cost;
 	private String image;
 	@ManyToOne
